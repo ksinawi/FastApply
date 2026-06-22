c697b481b976c5b192c5a25495c81b5ef565508a
Faisal Isam <fisam@uoguelph.ca>
2026-06-22 15:27:27 -0400

    Frontend Setup

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from routers import resume_router
 from routers import job_router
@@ -7,6 +8,14 @@ from routers import tailor_router
 from routers import process_application_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(resume_router.router)
 app.include_router(job_router.router)
